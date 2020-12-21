751a5bbfefb2737b3d083aa5a524e09e8f915850
tuananhvu319 <69627088+tuananhvu319@users.noreply.github.com>
2020-12-21 09:58:22 +0700

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import {MainContentComponent} from './components/main-content/main-content.component';
+import {AllProductsComponent} from './components/all-products/all-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainContentComponent
+  },
+  {
+    path: 'products/all-products',
+    component: AllProductsComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllProductsComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
